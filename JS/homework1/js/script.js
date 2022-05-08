d592eb6b50cb6f0ce27ef46878c320ac594bde37
igor4 <igor4uk69.7@gmail.com>
2022-05-08 19:01:24 +0300

    aaa

---
+++ JS/homework1/js/script.js
@@ -0,0 +1,20 @@
+//task 1
+let city = `Kyiv`;
+city = `Lviv`;
+let address = city;
+alert(address);
+
+
+//task 2
+let n = 5;
+let result = Math.pow(n, 3);
+console.log(result);
+
+
+//task 3
+let pen = 4;
+let pencil = 6;
+let pricePen = 5.25;
+let pricePencil = 3.50;
+let cost = pen*pricePen+pencil*pricePencil;
+document.write(cost);
